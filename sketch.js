8bf94d1f289243fdb57a5a6de5dc15ccba79b70b
Ayushi Kaushal <ayushikaushal@Ayushis-MacBook-Air.local>
2020-05-02 13:48:01 +0530

    adding constraint

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var platform2;
+var platform2, log6;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -38,6 +40,9 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log6 = new Log(230, 180, 80, PI/2);
+
+    chain = new Chain(bird.body, log6.body);
 }
 
 function draw(){
@@ -64,4 +69,6 @@ function draw(){
     bird.display();
     platform.display();
     platform2.display();
+    log6.display();
+    chain.display();
 }
